bcdfb851b783eef4b69447c580cd44005f4ec4e8
Piyush Gupta <piyushgupta428@gmail.com>
2021-01-08 17:33:51 -0600

    some necessary changes to index

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
